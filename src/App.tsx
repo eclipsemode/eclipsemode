cb13ae907d604d865d4f6eb8db82bfd9145242a6
Данил Волконеско <eclipsemod555@gmail.com>
2023-07-22 14:51:25 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,30 @@ function App() {
             <div><span className="type">const</span> <span className="var">me</span> = &#123;</div>
             <div><span className="param">  firstName</span>: <span className="string">'Daniel'</span>,</div>
             <div><span className="param">  lastName</span>: <span className="string">'Volkonesko'</span>,</div>
-            <div><span className="param">  age</span>: <span className="number">29</span>,</div>
+            <div><span className="param">  age</span>: <span className="number">30</span>,</div>
             <div><span className="param">  sex</span>: <span className="string">'male'</span>,</div>
-            <div><span className="param">  city</span>: <span className="string">'Armavir'</span>,</div>
             <br/>
             <div><span className="param">  technicalSkills</span>: [</div>
             <div>    <span className="string">'JavaScript'</span>, <span className="string">'TypeScript'</span>, <span className="string">'React'</span>,</div>
-            <div>    <span className="string">'HTML'</span>, <span className="string">'CSS'</span>, <span className="string">'Pug'</span>,</div>
+            <div>    <span className="string">'HTML'</span>, <span className="string">'CSS'</span>, <span className="string">'Pug'</span>, <span className="string">'Docker'</span>, <span className="string">'MobX'</span>,</div>
             <div>    <span className="string">'SASS'</span>, <span className="string">'Redux Toolkit'</span>, <span className="string">'PostgreSQL'</span>,</div>
-            <div>    <span className="string">'Webpack'</span>, <span className="string">'GIT'</span>, <span className="string">'Figma'</span></div>
+            <div>    <span className="string">'Webpack'</span>, <span className="string">'GIT'</span>, <span className="string">'Figma'</span>, <span className="string">'Node.js'</span>,</div>
+            <div>    <span className="string">'Express'</span>, <span className="string">'Next.js'</span>, <span className="string">'Sequelize'</span></div>
+            <div>    <span className="string">'Storybook'</span></div>
             <div>  ],</div>
             <br/>
             <div><span className="param">  familiarWith</span>: [</div>
-            <div>    <span className="string">'Node.js'</span>,</div>
-            <div>    <span className="string">'Express'</span>,</div>
             <div>    <span className="string">'Nest.js'</span>,</div>
-            <div>    <span className="string">'Next.js'</span>,</div>
-            <div>    <span className="string">'Docker'</span>,</div>
-            <div>    <span className="string">'C'</span></div>
+            <div>    <span className="string">'Ubuntu Linux'</span>,</div>
+            <div>    <span className="string">'Nginx'</span>,</div>
+            <div>    <span className="string">'C'</span>,</div>
+            <div>    <span className="string">'Jest'</span>,</div>
+            <div>    <span className="string">'CI/CD'</span></div>
             <div>  ],</div>
             <br/>
             <div><span className="param">  commonSkills</span>: [</div>
             <div>    <span className="string">'Hard work'</span>, <span className="string">'Learnability'</span>, <span className="string">'Discipline'</span>,</div>
-            <div>    <span className="string">'Curiosity'</span></div>
+            <div>    <span className="string">'Curiosity'</span>, <span className="string">'Self-learning'</span></div>
             <div>  ],</div>
             <br/>
             <div><span className="param">  acceptable</span>: &#123;</div>
@@ -45,7 +46,8 @@ function App() {
             <div>  &#125;,</div>
             <br/>
             <div><span className="param">  desiredPositions</span>: [</div>
-            <div>    <span className="string">'Frontend Developer'</span></div>
+            <div>    <span className="string">'Frontend Developer'</span>,</div>
+            <div>    <span className="string">'Full-stack Developer'</span>,</div>
             <div>  ],</div>
             <br/>
             <div><span className="param">mail</span>: <span className="string">'eclipsemod555@gmail.com'</span>,</div>
